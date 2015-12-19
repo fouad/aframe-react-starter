0fd0f0586909d77c44fdd74d00d542d13c5448aa
Fouad Matin <fm@fouad.co>
2015-12-18 19:40:44 -0800

    update relative paths

--- bundle.js
+++ bundle.js
@@ -153,7 +153,7 @@
 	      var boxes = this.rings.map(this.generateRing.bind(this));
 
 	      // <VideoSphere
-	      //       src={cdn(Math.random() < .5 ? 'https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/' : '/assets/videos/overpass.mp4')}
+	      //       src={cdn(Math.random() < .5 ? 'https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/' : 'assets/videos/overpass.mp4')}
 	      //       muted={true}
 	      //       position="50 0 15"
 	      //       rotation="0 -90 0"/>
@@ -170,7 +170,7 @@
 	        _react2.default.createElement(_primitives.Light, { type: 'directional', intensity: '0.5', position: '0 0 -1' }),
 	        _react2.default.createElement(_primitives.Light, { type: 'directional', intensity: '1', position: '0 0 -1' }),
 	        _react2.default.createElement(_primitives.CurvedImage, {
-	          src: (0, _utils.cdn)('/assets/images/wikipage.png'),
+	          src: (0, _utils.cdn)('assets/images/wikipage.png'),
 	          position: '0 2 0',
 	          radius: '5.7',
 	          thetaStart: '30',
